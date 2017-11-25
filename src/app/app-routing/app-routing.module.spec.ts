409bd86a5e682a79046b829c6be6a0fbb49a230b
Elke Heymans <elke@Elkes-MacBook-Pro-2.local>
2017-11-25 22:39:56 +0100

    Simplified the unit test for the app routing

--- src/app/app-routing/app-routing.module.spec.ts
+++ src/app/app-routing/app-routing.module.spec.ts
@@ -4,23 +4,8 @@ import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
-import { Project365Component } from '../project365/project365.component';
+import { AppModule } from '../app.module';
 import { AppComponent } from '../app.component';
-import { Project365Module } from '../project365/project365.module';
-import { HomeModule } from '../home/home.module';
-import { HomeComponent } from '../home/home.component';
-import { EventsComponent } from '../events/events.component';
-import { PartiesModule } from '../parties/parties.module';
-import { PhotoshootsModule } from '../photoshoots/photoshoots.module';
-import { EventsModule } from '../events/events.module';
-import { WeddingsModule } from '../weddings/weddings.module';
-import { ContactModule } from '../contact/contact.module';
-
-@Component({ selector: 'app-root', template: '<router-outlet></router-outlet>' })
-class MockAppComponent { }
-
-@Component({ selector: 'project365', template: '' })
-class MockProject365Component { }
 
 describe('AppRoutingModule', () => {
     let appRoutingModule: AppRoutingModule;
@@ -34,16 +19,7 @@ describe('AppRoutingModule', () => {
         TestBed.configureTestingModule({
         imports: [ 
             RouterTestingModule.withRoutes(AppRoutingModule.getRoutes()),
-            Project365Module,
-            HomeModule,
-            EventsModule,
-            PartiesModule,
-            PhotoshootsModule,
-            WeddingsModule,
-            ContactModule
-        ],
-        declarations: [
-            AppComponent
+            AppModule
         ]
         });
 
