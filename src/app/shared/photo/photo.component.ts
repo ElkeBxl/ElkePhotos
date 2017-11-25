a9f92b29860bf915ad11436b49f1670aa234172d
Elke Heymans <elke@Elkes-MacBook-Pro-2.local>
2017-11-25 14:11:42 +0100

    List of 365 photos is rendered with the new PhotoComponent. Nothing fancy yet, just basic setup

--- src/app/shared/photo/photo.component.ts
+++ src/app/shared/photo/photo.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Photo } from '../../models/photo';
 
 @Component({
   selector: 'photo',
@@ -6,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./photo.component.scss']
 })
 export class PhotoComponent implements OnInit {
+    @Input() photo: Photo;
+    
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+    }
 }
