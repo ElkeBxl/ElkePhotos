c4ca257971a9305afc2332efa073915242ddec94
Elke Heymans <elke@Elkes-MacBook-Pro-2.local>
2017-12-10 17:40:38 +0100

    Added smooth scrolling to the InPageDirective

--- src/app/shared/in-page-navigation.directive.ts
+++ src/app/shared/in-page-navigation.directive.ts
@@ -1,11 +1,11 @@
-import { Directive, ViewChild, ElementRef } from '@angular/core';
+import { Directive, ViewChild, ElementRef, Renderer } from '@angular/core';
 
 @Directive({
     selector: '[inpagenavigation]'
 })
 export class InPageNavigationDirective {
 
-    constructor(private pagenavigation: ElementRef) { }
+    constructor(private pagenavigation: ElementRef, private renderer: Renderer) { }
 
     ngOnInit() {
     }
@@ -14,9 +14,17 @@ export class InPageNavigationDirective {
         let elements = (<HTMLElement>this.pagenavigation.nativeElement).querySelectorAll('a');
         for(let i = 0; i < elements.length; i++) {
             let element = elements[i];
-            element.onclick = (el) => { 
+            element.onclick = (event) => { 
+                event.preventDefault();
                 this.removeAllActiveClasses();
-                el.srcElement.parentElement.classList.add('active');
+                event.srcElement.parentElement.classList.add('active');
+                let target = event.srcElement.attributes['href'].value;
+                let targetElement = <HTMLElement>document.querySelector(target);
+                window.scrollTo({ 
+                    left: targetElement.clientLeft, 
+                    top: targetElement.offsetTop,
+                    behavior: 'smooth'
+                });
             };
         }
     }
@@ -28,5 +36,4 @@ export class InPageNavigationDirective {
             element.parentElement.classList.remove('active');
         }
     }
-
 }
